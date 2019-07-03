83a9fcc88c7838e184da41d282469af36a3547f0
wangxiaoc6 <wangxiaoc6@qq.com>
2019-07-03 14:22:20 +0800

    repair bug （common.py ,params.py ,error 404: Not Found,error:No such file or directory）

--- package/scripts/common.py
+++ package/scripts/common.py
@@ -54,11 +54,14 @@ def download_hue():
   """
   Download Hue to the installation directory
   """
-  Execute('{0} | xargs wget -O hue.tgz'.format(params.download_url))
-  Execute('tar -zxvf hue.tgz -C {0} && rm -f hue.tgz'.format(params.hue_install_dir))
+  Execute('wget {0}'.format(params.download_url))
+  Execute('tar -zxvf hue-3.11.0.tgz -C {0} && rm -f hue-3.11.0.tgz'.format(params.hue_install_dir))
   # Ensure all Hue files owned by hue
-  Execute('chown -R {0}:{1} {2}'.format(params.hue_user,params.hue_group,params.hue_dir))
+  Execute('mkdir -p {0}'.format(params.hue_dir))
   Execute('ln -s {0} /usr/hdp/current/hue-server'.format(params.hue_dir))
+  Execute('rm -rf {0}/*'.format(params.hue_dir))
+  Execute('cd /usr/local/hue-3.11.0 && make install')
+  Execute('chown -R {0}:{1} {2}'.format(params.hue_user, params.hue_group, params.hue_dir))
   Logger.info("Hue Service is installed")
 
 def add_hdfs_configuration(if_ranger=False, security_enabled=False):
